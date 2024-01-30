0f910201a6821898a19461344eeaeb1709f177a2
Khoana <nakhoa280503@gmail.com>
2024-01-30 09:26:43 +0700

    User Model

---
+++ app/src/main/java/com/example/eu_fstyle_mobile/src/model/Client.java
@@ -0,0 +1,17 @@
+package com.example.eu_fstyle_mobile.src.model;
+
+import java.lang.reflect.Array;
+
+public class Client {
+    private String email;
+    private String password;
+    private String username;
+    private String avatar;
+    private Boolean isActive;
+    private String token;
+    private Array cart;
+    private String address;
+    private String phoneNumber;
+    private String receiverName;
+    private String isAdmin;
+}
