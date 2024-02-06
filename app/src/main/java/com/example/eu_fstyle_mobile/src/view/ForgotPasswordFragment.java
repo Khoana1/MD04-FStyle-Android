2f9e36d1e28da82e4eb7c46207807fce25c4afbb
Khoana <nakhoa280503@gmail.com>
2024-02-06 14:40:03 +0700

    create register user

--- app/src/main/java/com/example/eu_fstyle_mobile/src/view/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/eu_fstyle_mobile/src/view/ForgotPasswordFragment.java
@@ -15,6 +15,8 @@ import com.example.eu_fstyle_mobile.R;
 import com.example.eu_fstyle_mobile.databinding.FragmentForgotPasswordBinding;
 import com.example.eu_fstyle_mobile.src.base.BaseFragment;
 
+import java.util.regex.Pattern;
+
 public class ForgotPasswordFragment extends BaseFragment<FragmentForgotPasswordBinding> {
 
     @Override
@@ -25,11 +27,118 @@ public class ForgotPasswordFragment extends BaseFragment<FragmentForgotPasswordB
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        setStatusHelperText();
+        binding.icGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+        binding.btnChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateChangePass();
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentForgotPasswordBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    private void setStatusHelperText() {
+        binding.emailLayout.setHelperText("");
+        binding.phoneLayout.setHelperText("");
+        binding.passwordLayout.setHelperText("");
+        binding.newPassLayout.setHelperText("");
+    }
+
+    private void validateChangePass() {
+        validateEmail();
+        validatePhone();
+        validatePassword();
+        validateRePass();
+    }
+
+    private void validateEmail() {
+        String helperText = "";
+        String email = binding.edtEmail.getText().toString();
+
+        if (email.isEmpty()) {
+            helperText = "Không được để trống Email";
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            helperText = "Vui lòng nhập đúng định dạng Email (ví dụ: example@gmail.com) để tiếp tục";
+        }
+
+        binding.emailLayout.setHelperText(helperText);
+    }
+
+    private void validatePassword() {
+        String helperText = "";
+        String password = binding.edtPass.getText().toString();
+        Pattern upperCasePattern = Pattern.compile(".*[A-Z].*");
+        Pattern lowerCasePattern = Pattern.compile(".*[a-z].*");
+        Pattern specialCharPattern = Pattern.compile(".*[@#\\$%^&+=].*");
+
+        if (password.isEmpty()) {
+            helperText = "Không được để trống mật khẩu";
+        } else if (password.length() < 8) {
+            helperText = "Mật khẩu phải có ít nhất 8 ký tự";
+        } else if (!upperCasePattern.matcher(password).matches()) {
+            helperText = "Mật khẩu phải có ít nhất 1 ký tự viết hoa";
+        } else if (!lowerCasePattern.matcher(password).matches()) {
+            helperText = "Mật khẩu phải có ít nhất 1 ký tự viết thường";
+        } else if (!specialCharPattern.matcher(password).matches()) {
+            helperText = "Mật khẩu phải có ít nhất 1 ký tự đặc biệt";
+        }
+
+        binding.passwordLayout.setHelperText(helperText);
+
+    }
+
+    private void validatePhone() {
+        String helperText = "";
+        String phone = binding.edtPhone.getText().toString();
+        if (phone.isEmpty()) {
+            helperText = "Không được để trống số điện thoại";
+        } else if (!phone.matches(".*[0-9].*")) {
+            helperText = "Số điện thoại phải là số";
+        } else if (phone.length() != 10) {
+            helperText = "Số điện thoại phải có 10 số";
+        }
+
+        binding.phoneLayout.setHelperText(helperText);
+
+    }
+
+    private void validateRePass() {
+        String helperText = "";
+        String rePassword = binding.edtNewPass.getText().toString();
+        String password = binding.edtPass.getText().toString();
+        Pattern upperCasePattern = Pattern.compile(".*[A-Z].*");
+        Pattern lowerCasePattern = Pattern.compile(".*[a-z].*");
+        Pattern specialCharPattern = Pattern.compile(".*[@#\\$%^&+=].*");
+
+        if (rePassword.isEmpty()) {
+            helperText = "Không được để trống mật khẩu";
+        } else if (rePassword.length() < 8) {
+            helperText = "Mật khẩu phải có ít nhất 8 ký tự";
+        } else if (!upperCasePattern.matcher(rePassword).matches()) {
+            helperText = "Mật khẩu phải có ít nhất 1 ký tự viết hoa";
+        } else if (!lowerCasePattern.matcher(rePassword).matches()) {
+            helperText = "Mật khẩu phải có ít nhất 1 ký tự viết thường";
+        } else if (!specialCharPattern.matcher(rePassword).matches()) {
+            helperText = "Mật khẩu phải có ít nhất 1 ký tự đặc biệt";
+        } else if (!rePassword.equals(password)) {
+            helperText = "Mật khẩu không trùng khớp! Vui lòng nhập lại";
+        }
+
+        binding.newPassLayout.setHelperText(helperText);
     }
 }
--- app/src/main/java/com/example/eu_fstyle_mobile/src/view/SplashFragment.java
+++ app/src/main/java/com/example/eu_fstyle_mobile/src/view/SplashFragment.java
@@ -1,5 +1,6 @@
 package com.example.eu_fstyle_mobile.src.view;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,7 +11,10 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
+import com.example.eu_fstyle_mobile.R;
 import com.example.eu_fstyle_mobile.databinding.FragmentSplashFragmetBinding;
 import com.example.eu_fstyle_mobile.src.base.BaseFragment;
 
@@ -24,6 +28,12 @@ public class SplashFragment extends BaseFragment<FragmentSplashFragmetBinding> {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(binding.imgLogo,"translationY",-100f,0f);
+        animator.setDuration(2000);
+        animator.setRepeatCount(0);
+        animator.start();
+        Animation animationLogo = AnimationUtils.loadAnimation(getActivity(), R.anim.alpha);
+        binding.imgLogo.startAnimation(animationLogo);
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             openScreen(new LoginFragment(), false);
         }, 3000); // vào ứng dụng sau 3s
