844508561aa7f9e35ae578fa56b2013aa08c277b
Khoana <nakhoa280503@gmail.com>
2024-03-26 10:00:30 +0700

    fix image favourite cart

--- app/src/main/java/com/example/eu_fstyle_mobile/src/adapter/CartAdapter.java
+++ app/src/main/java/com/example/eu_fstyle_mobile/src/adapter/CartAdapter.java
@@ -1,11 +1,15 @@
 package com.example.eu_fstyle_mobile.src.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.eu_fstyle_mobile.R;
 import com.example.eu_fstyle_mobile.databinding.ItemCartProductBinding;
 import com.example.eu_fstyle_mobile.databinding.ItemFavouriteBinding;
 import com.example.eu_fstyle_mobile.src.model.ProductCart;
@@ -31,7 +35,22 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ProductCart productCart = productCartList.get(position);
         String image = productCart.getImageDefault();
-        Picasso.get().load(image).into(holder.binding.imgShoe);
+        if (image != null) {
+            if (image.startsWith("http")) {
+                Picasso.get().load(image)
+                        .error(R.drawable.error_shoe)
+                        .into(holder.binding.imgShoe);
+            } else {
+                boolean isBase64 = isBase64(image);
+                if (isBase64) {
+                    byte[] decodedString = Base64.decode(image, Base64.DEFAULT);
+                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    holder.binding.imgShoe.setImageBitmap(decodedByte);
+                } else {
+                    holder.binding.imgShoe.setImageResource(R.drawable.error_shoe);
+                }
+            }
+        }
         String defaultName = productCart.getName();
         int maxLength = 20;
         if (defaultName.length() > maxLength) {
@@ -85,6 +104,18 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder> {
         public ViewHolder(@NonNull ItemCartProductBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+        }
+    }
+
+    public static boolean isBase64(String input) {
+        try {
+            byte[] decodedBytes = Base64.decode(input, Base64.DEFAULT);
+            String decodedString = new String(decodedBytes);
+            byte[] reEncodedBytes = Base64.encode(decodedString.getBytes(), Base64.DEFAULT);
+            String reEncodedString = new String(reEncodedBytes);
+            return reEncodedString.equals(input);
+        } catch (IllegalArgumentException e) {
+            return false;
         }
     }
 }
--- app/src/main/java/com/example/eu_fstyle_mobile/src/adapter/MyFavouriteAdapter.java
+++ app/src/main/java/com/example/eu_fstyle_mobile/src/adapter/MyFavouriteAdapter.java
@@ -1,5 +1,8 @@
 package com.example.eu_fstyle_mobile.src.adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -23,10 +26,11 @@ public class MyFavouriteAdapter extends RecyclerView.Adapter<MyFavouriteAdapter.
     public MyFavouriteAdapter(List<ProductFavourite> productList) {
         this.productList = productList;
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyFavouriteAdapter .ViewHolder(ItemFavouriteBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+        return new MyFavouriteAdapter.ViewHolder(ItemFavouriteBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
     }
 
     @Override
@@ -35,11 +39,23 @@ public class MyFavouriteAdapter extends RecyclerView.Adapter<MyFavouriteAdapter.
         String image = product.getDefaultImage();
         holder.binding.itemNameProductHome.setText(product.getName());
         DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        holder.binding.itemPriceProductHome.setText(decimalFormat.format(product.getPrice())+" VNĐ");
-        Picasso.get().load(image)
-                .placeholder(R.drawable.icon_home)
-                .error(R.drawable.icon_erro)
-                .into(holder.binding.itemImageProductHome);
+        holder.binding.itemPriceProductHome.setText(decimalFormat.format(product.getPrice()) + " VNĐ");
+        if (image != null) {
+            if (image.startsWith("http")) {
+                Picasso.get().load(image)
+                        .error(R.drawable.error_shoe)
+                        .into(holder.binding.itemImageProductHome);
+            } else {
+                boolean isBase64 = isBase64(image);
+                if (isBase64) {
+                    byte[] decodedString = Base64.decode(image, Base64.DEFAULT);
+                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    holder.binding.itemImageProductHome.setImageBitmap(decodedByte);
+                } else {
+                    holder.binding.itemImageProductHome.setImageResource(R.drawable.error_shoe);
+                }
+            }
+        }
     }
 
     @Override
@@ -53,6 +69,18 @@ public class MyFavouriteAdapter extends RecyclerView.Adapter<MyFavouriteAdapter.
         public ViewHolder(@NonNull ItemFavouriteBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+        }
+    }
+
+    public static boolean isBase64(String input) {
+        try {
+            byte[] decodedBytes = Base64.decode(input, Base64.DEFAULT);
+            String decodedString = new String(decodedBytes);
+            byte[] reEncodedBytes = Base64.encode(decodedString.getBytes(), Base64.DEFAULT);
+            String reEncodedString = new String(reEncodedBytes);
+            return reEncodedString.equals(input);
+        } catch (IllegalArgumentException e) {
+            return false;
         }
     }
 }
